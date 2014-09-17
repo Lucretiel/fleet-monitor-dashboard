ff06a7ff4580a026de5e31ef3879a94c0231b57a
Nathan West <Lucretiel@gmail.com>
2014-09-16 20:45:03 -0400

    Updated app things

--- app.js
+++ app.js
@@ -4,28 +4,51 @@
     //TODO: figure out how to make the websocket a service
     app.controller('FleetMonitorController', ['$scope', function($scope) {
         var websocket = null;
+        var state = 0;
 
-        $scope.state = 0;
+        var fields = [];
+        var items = [];
 
-        $scope.fields = [];
-        $scope.items = [];
+        $scope.is_disconnected = function() {
+            return state == 0;
+        }
+
+        $scope.is_connecting = function() {
+            return state == 1;
+        }
+
+        $scope.is_connected = function() {
+            return state == 2;
+        }
+
+        $scope.fields = function() {
+            return fields;
+        }
+
+        $scope.items = function() {
+            return items;
+        }
+
+        $scope.has_data = function() {
+            return !$scope.is_disconnected() && fields.length;
+        }
 
         $scope.disconnect = function() {
             $scope.$apply(function() {
-                $scope.state = 0;
-                $scope.fields = [];
-                $scope.items = [];
+                state = 0;
+                fields = [];
+                items = [];
             });
             websocket = null;
         };
 
         $scope.connect_to = function(url) {
-            $scope.state = 1;
+            state = 1;
 
             websocket = new WebSocket(url);
 
             websocket.onopen = function() {
-                $scope.$apply(function() { $scope.state = 2; });
+                $scope.$apply(function() { state = 2; });
             };
 
             websocket.onclose = function() {
@@ -39,11 +62,10 @@
             websocket.onmessage = function(evt) {
                 var message = JSON.parse(evt.data);
                 $scope.$apply(function() {
-                    $scope.state = 2;
                     if(message.type == "unit")
                     {
-                        $scope.fields = message.fields;
-                        $scope.items = message.items;
+                        fields = message.fields;
+                        items = message.items;
                     }
                 });
             };
